579791ffab1b3dffba4853cdb8dcd6cbf21ce90c
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2011-02-28 23:52:55 +0100

    Handle status codes and initFromXmlFile

--- override.py
+++ override.py
@@ -8,6 +8,25 @@ class Context(_Ctype):
                 return None
             if isinstance(i, _Ints):
                 return _Cobject(cls, ctypes.c_void_p(i))
+            elif isinstance(i, basestring):
+                # Init from XML file
+                p = ctypes.c_void_p()
+                err = EnumerationErrors()
+                status = dll.xnInitFromXmlFile(i, p, err)
+                if status:
+                    raise Exception("Error %s: %s" % (dll.xnGetStatusName(status),
+                                                      dll.xnGetStatusString(status)))
+
         p = ctypes.c_void_p()
-        dll.xnInit(ctypes.byref(p))
+        status = dll.xnInit(ctypes.byref(p))
+        if status:
+            raise Exception("Error %s: %s" % (dll.xnGetStatusName(status),
+                                              dll.xnGetStatusString(status)))
         return _Cobject(cls, p)
+
+    def StatusName(self, status):
+        return dll.xnGetStatusName(status)
+
+    def StatusString(self, status):
+        return dll.xnGetStatusString(status)
+
