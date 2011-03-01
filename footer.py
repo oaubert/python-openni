279a1d3bb53a3ee29155d5ad40dc276baefe1ceb
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2011-03-01 19:11:09 +0100

    Update example

--- footer.py
+++ footer.py
@@ -1,29 +1,42 @@
 
 # Start of footer.py #
 
-def debug_callback(event, *args, **kwds):
+def error(s):
+    xnPrintError(s, "OpenNI error")
+
+def debug(*args):
     '''Example callback, useful for debugging.
     '''
-    l = ['event %s' % (event.type,)]
-    if args:
-        l.extend(map(str, args))
-    if kwds:
-        l.extend(sorted('%s=%s' % t for t in kwds.items()))
-    print('Debug callback (%s)' % ', '.join(l))
+    print('Debug callback (%s)' % ', '.join(args))
 
 if __name__ == '__main__':
+    import time
+
+    #c = Context('/home/oaubert/src/kinect/Nite-1.3.0.18/Data/Sample-User.xml')
     c = Context()
+    q=NodeQuery()
     err = EnumerationErrors()
 
-    q=NodeQuery()
-    q.SetCreationInfo("Foo")
-    q.AddSupportedCapability("Mirror")
-    output = MapOutputMode(640, 480, 30)
-    q.AddSupportedMapOutputMode(output)    
-    c.CreateDepthGenerator(q, err)
+    #q.setCreationInfo("Foo")
+    #q.addSupportedCapability("Mirror")
+    #output = MapOutputMode(640, 480, 30)
+    #q.addSupportedMapOutputMode(output)    
+    #c.createDepthGenerator(q, err)
     
-    u = c.CreateUserGenerator(q, err)
-    if not u.IsCapabilitySupported('User::Skeleton'):
+    u=c.createUserGenerator(q, err)
+    if not u.isCapabilitySupported('User::Skeleton'):
         raise "Unable to create UserGenerator"
+    h=u.registerUserCallbacks(cb.UserHandler(debug), cb.UserHandler(debug), "User")
+    print "Registered cb %x" % h
+    s=c.startGeneratingAll()
+    if s:
+        error(s)
+    for i in range(4):
+        s=c.waitNoneAndUpdateAll()
+        if s:
+            error(s)
+        print "Update", u.getUsers()
+        time.sleep(.1)
+
 
     
